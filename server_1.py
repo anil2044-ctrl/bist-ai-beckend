b29624f2ba3a2c4b152ceaa3f0bc67af7eff4ade
anil2044-ctrl <2544002013@ogr.cbu.edu.tr>
2026-03-11 18:12:35 +0300

    Update server_1.py

--- server_1.py
+++ server_1.py
@@ -110,4 +110,5 @@ def get_tickers():
 if __name__ == "__main__":
     print("🚀 BIST Backend başlatılıyor: http://localhost:5000")
     import os
-    port = int(os.environ.get('PORT', 500 app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
